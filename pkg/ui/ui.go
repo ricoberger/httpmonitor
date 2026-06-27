479b53ef7ea57846d256ef0f6b9913122bb21507
ricoberger <mail@ricoberger.de>
2026-06-27 07:49:28 +0200

    chore(deps): update bubbletea and lipgloss to v2

--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -3,16 +3,13 @@ package ui
 import (
 	"github.com/ricoberger/httpmonitor/pkg/target"
 
-	tea "github.com/charmbracelet/bubbletea"
+	tea "charm.land/bubbletea/v2"
 )
 
 func Start(targets []target.Client) error {
 	model := NewModel(targets)
 
-	p := tea.NewProgram(
-		model,
-		tea.WithAltScreen(),
-	)
+	p := tea.NewProgram(model)
 	if _, err := p.Run(); err != nil {
 		return err
 	}
